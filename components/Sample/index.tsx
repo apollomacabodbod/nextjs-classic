09d663d4b9293ee87bd2d89d7f71f69ea9e0d170
apollo <apollomacabodbod11@gmail.com>
2024-08-03 09:05:23 +0800

    test component

--- components/Sample/index.tsx
+++ components/Sample/index.tsx
@@ -1,12 +1,20 @@
 
-
 export default function Sample(){
 
   return(
     
     <>
 
-      <p>Hello</p>
+
+      <div className="flex flex-col border-[#50B498] ">
+
+        <div className="flex items-center justify-center border-[#50B498]">
+
+          <p className="border border-[#50B498] ">Hello World</p>
+
+        </div>
+      </div>
+    
     
     </>
 
